f0b795202f99cc535a8d71667ef2eb91b61a0e80
Gerson Gallo <ggallovalle@gmail.com>
2026-05-04 06:35:58 -0600

    feat(program/niri): niri and noctalia base config

--- machine/cachyos/casa.lua
+++ machine/cachyos/casa.lua
@@ -1,27 +1,55 @@
+local H = {}
+
+function H.entry_config_dir(program, attributes)
+  local entry = {
+    type = "directory",
+    source = { program, "config", program },
+    target = { ".config", program }
+  }
+  if type(attributes) == "table" then
+    entry.attributes = attributes
+  end
+
+  return entry
+end
+
+function H.entry_home_file(program, source, target, attributes)
+  local entry = {
+    type = "file",
+    source = { program, "home", source },
+    target = { target or source }
+  }
+  if type(attributes) == "table" then
+    entry.attributes = attributes
+  end
+
+  return entry
+end
+
+function H.entry_config_file(program, source, target, attributes)
+  local entry = {
+    type = "file",
+    source = { program, "config", program, source },
+    target = { ".config", program, target or source }
+  }
+  if type(attributes) == "table" then
+    entry.attributes = attributes
+  end
+
+  return entry
+end
+
 return {
   entries = {
     -- zsh
-    {
-      type = "directory",
-      source = { "zsh", "config", "zsh" },
-      target = { ".config", "zsh" }
-    },
-    {
-      type = "file",
-      source = { "zsh", "home", "zshrc" },
-      target = { ".zshrc" }
-    },
-    {
-      type = "file",
-      source = { "zsh", "home", "zshenv" },
-      target = { ".zshenv" }
-    },
+    H.entry_config_dir("zsh"),
+    H.entry_home_file("zsh", "zshrc", ".zshrc"),
+    H.entry_home_file("zsh", "zshenv", ".zshenv"),
     -- git
-    {
-      type = "file",
-      source = { "git", "config", "git", "config" },
-      target = { ".config", "git", "config" },
-      attributes = { "template" }
-    },
+    H.entry_config_file("git", "config", nil, { "template" }),
+    -- gui
+    -- niri
+    H.entry_config_dir("niri"),
+    H.entry_config_dir("noctalia"),
   }
 }
