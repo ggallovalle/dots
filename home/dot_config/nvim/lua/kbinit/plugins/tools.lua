22e7c89190f480d5ea27b905cec50a2836cf7ead
Gerson Gallo <ggallovalle@gmail.com>
2026-06-06 01:48:47 -0600

    feat(nvim): oil dont show gitignored files

--- home/dot_config/nvim/lua/kbinit/plugins/tools.lua
+++ home/dot_config/nvim/lua/kbinit/plugins/tools.lua
@@ -1,6 +1,8 @@
 ---@type LazyPluginSpec
 local Opencode = { url = "https://github.com/nickjvandyke/opencode.nvim.git" }
 
+local show_gitignored = false
+
 ---@type LazyPluginSpec
 local Oil = {
   "stevearc/oil.nvim",
@@ -8,9 +10,33 @@ local Oil = {
   init = function ()
     vim.g.loaded_netrwPlugin = 1
   end,
-  opts = {
-    default_file_explorer = true,
-  }
+  opts = function()
+    return {
+      default_file_explorer = true,
+      view_options = {
+        is_always_hidden = function(name, bufnr)
+          if show_gitignored then
+            return false
+          end
+          local dir = require("oil").get_current_dir(bufnr)
+          if not dir then
+            return false
+          end
+          local ret = vim.fn.system({ "git", "-C", dir, "check-ignore", name })
+          return vim.v.shell_error == 0
+        end,
+      },
+      keymaps = {
+        ["gi"] = {
+          callback = function()
+            show_gitignored = not show_gitignored
+            require("oil.actions").refresh.callback()
+          end,
+          desc = "Toggle gitignored files",
+        },
+      },
+    }
+  end,
 }
 
 return { Opencode, Oil }
