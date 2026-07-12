34dcdfd99f3952b349852224ef3dda517bfd1ec0
Gerson Gallo <ggallovalle@gmail.com>
2026-07-12 10:00:57 -0600

    feat: configure elixir lsp, mason, conform, treesitter

--- home/dot_config/nvim/lua/kbinit/languages/elixir.lua
+++ home/dot_config/nvim/lua/kbinit/languages/elixir.lua
@@ -1,6 +1,26 @@
 return {
+  lsp = {
+    elixirls = {
+      filetypes = { "elixir", "eelixir", "heex" },
+      root_markers = { "mix.exs", ".git" },
+      settings = {
+        elixirls = {
+          dialyzerEnabled = false,
+          enableTestLenses = false
+        }
+      }
+    }
+  },
+  mason = { "elixirls" },
   treesitter = {
     "elixir",
-    "eex"
+    "eex",
+    "heex"
+  },
+  conform = {
+    formatters_by_ft = {
+      elixir = { "mix" },
+      heex = { "mix" }
+    }
   }
 }
