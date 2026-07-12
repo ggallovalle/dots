914b9e19578468ece4108a92d59438e4bf74fe3b
Gerson Gallo <ggallovalle@gmail.com>
2026-07-12 10:00:34 -0600

    feat: auto-detect uv run --script files as python

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,2 +1,10 @@
 require("kbinit").setup()
 -- Im gerson, or am I
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  pattern = "*",
+  callback = function()
+    if vim.fn.getline(1):match("^#!.*uv%s+run%s+--script") then
+      vim.bo.filetype = "python"
+    end
+  end,
+})
